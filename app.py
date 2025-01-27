7e59c2e6c7ac4538b33a159c1d47d4f1bb6e8b4e
kiady696 <kiadyrajwels.dark@gmail.com>
2025-01-27 02:45:16 +0100

    Ajout dossier pycache a ignorer + route '/'

--- app.py
+++ app.py
@@ -1,29 +1,15 @@
 from flask import Flask, render_template
-import pkg_resources
 
 devmode = True
 
-app = Flask(__name__,
-        static_folder = "../frontend/dist/assets",
-        template_folder = "../frontend/dist")
+app = Flask(__name__)
 
 
 
 @app.route('/')
 def homepage():
-    return render_template('index.html')
-
-@app.route('/api/python_pkg')
-def pkg():
-    installed_packages = pkg_resources.working_set
-    installed_packages_list = sorted(["%s  %s" % (i.key, i.version) for i in installed_packages])
-    return installed_packages_list
-
-
-@app.route('/api/mode')
-def getMode():
-    return {"devmode":devmode}
+    return 'Projet Famoki'
 
 
 if __name__ == '__main__':
-    app.run(port=4000,debug=devmode)
+    app.run(port=5000,debug=devmode)
