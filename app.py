e23be5c452e2245b82c0e7611c8b256dc947e274
kiady696 <kiadyrajwels.dark@gmail.com>
2025-01-29 00:07:07 +0100

    Prepa authentification endpoint

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ app = Flask(__name__)
 def homepage():
     return 'Projet Famoki'
 
+@app.route('/login')
+def page_de_connexion():
+    return '/loginRouteVue'
+
 
 if __name__ == '__main__':
     app.run(port=5000,debug=devmode)
