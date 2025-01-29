f39201a9779c3c08c9ef625b5effdd5209a6a320
faresgst <gasti.fares444@gmail.com>
2025-01-29 15:35:38 +0100

    fares-backend_recuperationFichierCSV+name

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, jsonify
 import io
 import csv
 import pandas as pd
+import os
 
 
 devmode = True
@@ -9,33 +10,59 @@ devmode = True
 app = Flask(__name__)
 
 
+# Répertoire où les fichiers seront sauvegardés temporairement
+UPLOAD_FOLDER = 'uploads'
+
+# Crée le dossier uploads si il n'existe pas déjà
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 
 @app.route('/api/csv', methods=['POST'])
-
 def upload_csv():
-    file = request.files.get('file')  # Récupère le fichier
-    if file and file.filename.endswith('.csv'):
-        # Lire le contenu du fichier CSV
+        file = request.files.get('file')  # Récupère le fichier
+
+        if file and file.filename.endswith('.csv'): #Vérification si le fichier est bien un CSV
+
+            name_csv = file.filename # Get name csv
+
+        # Sauvegarder le fichier temporairement
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
+            file.save(file_path)
+
+        # Charger le fichier CSV dans un DataFrame Pandas
+            try:
+                df = pd.read_csv(file_path)
+            # Optionnel: tu peux ici faire des opérations sur le DataFrame
+                print(df.head())  # Affiche les 5 premières lignes du DataFrame
             
-        content = file.stream.read().decode('utf-8')
-        csv_reader = csv.reader(io.StringIO(content))
+            # Retourner un message de succès avec un aperçu des données
+                #afficher : nom du fichier, nom des colonnes,  target
+                return jsonify({"filename": name_csv, "data_preview": df.head().to_json()}), 200
+            except Exception as e:
+                return jsonify({"error": f"Error processing CSV: {str(e)}"}), 500
+        else:
+            return jsonify({"error": "Invalid file format, only CSV files are allowed"}), 400
+
+
+
         
-        # Optionnel : faire un traitement sur les données CSV
-        data = []
-        for row in csv_reader:
-            # Par exemple, ici on fait un traitement basique
-            processed_row = [field.upper() for field in row]  # Exemple de traitement (mettre en majuscule)
-            data.append(processed_row)
+         
 
-        # Retourner les données traitées
-    data = jsonify(data)
-    return data # Renvoie les données traitées au frontend
+        
 
-def get_target():
 
-    csv = upload_csv()
+
+
+
+
+
+
+
+
 
 
 
