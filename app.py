3477067f6b3bd2003688d581b11e8359965bf0ae
faresgst <gasti.fares444@gmail.com>
2025-01-29 10:00:14 +0100

    route api csv (input) FARES

--- app.py
+++ app.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import io
+import csv
+import jsonify
+
 
 devmode = True
 
@@ -6,10 +10,31 @@ app = Flask(__name__)
 
 
 
-@app.route('/')
-def homepage():
-    return 'Projet Famoki'
+
+
+@app.route('/api/csv', methods=['POST'])
+
+def upload_csv():
+    file = request.files.get('file')  # Récupère le fichier
+    if file and file.filename.endswith('.csv'):
+        # Lire le contenu du fichier CSV
+        content = file.stream.read().decode('utf-8')
+        csv_reader = csv.reader(io.StringIO(content))
+        
+        # Optionnel : faire un traitement sur les données CSV
+        data = []
+        for row in csv_reader:
+            # Par exemple, ici on fait un traitement basique
+            processed_row = [field.upper() for field in row]  # Exemple de traitement (mettre en majuscule)
+            data.append(processed_row)
+
+        # Retourner les données traitées
+    return jsonify(data)  # Renvoie les données traitées au frontend
+
+
+    
+
 
 
 if __name__ == '__main__':
-    app.run(port=5000,debug=devmode)
+    app.run(debug=True, host='127.0.0.1', port=5000)
